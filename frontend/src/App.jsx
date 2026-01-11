67d16cd4b5a4ffbede29b2cdcad2c52b540a0825
Shikhar-74 <shikharshukla74088@gmail.com>
2026-01-11 22:07:26 +0530

    frontend setup and make routes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import react from 'react'
+import {Route, Routes} from 'react-router-dom'
+import Home  from './pages/home'
+import UserLogin from './pages/UserLogin'
+import UserSignup from './pages/UserSignup'
+import Captainlogin  from './pages/CaptainLogin'
+import Captainsignup from './pages/CaptainSignup'
+const App = ()=>{
+  return (
+    <div>
+      <Routes>
+        <Route path= '/' element={<Home />} />
+        <Route path= '/login' element={<UserLogin/>}/>
+        <Route path= '/signup' element={<UserSignup/>}/>
+        <Route path= '/captain-login' element={<Captainlogin/>}/>
+        <Route path = '/captains-signup' element= {<Captainsignup/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
